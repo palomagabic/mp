3323c686f6f2218a64c01a4cecbf4330110f1732
palomagabic <p.corteslazo@gmail.com>
2019-10-14 20:55:19 -0300

    agregue el filter

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
   get 'pages/invitade'
   get 'pages/new'
   get 'pages/type'
+  get 'pages/filter'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
